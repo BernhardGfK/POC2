ea4118fa01069808e8a0ed9fdda14a8303bff919
bdschi <kadmus@nexgo.de>
2023-12-22 19:06:02 +0100

    group by was wrong

--- buyers.sql
+++ buyers.sql
@@ -1,12 +1,22 @@
-with buyers as (
+with cswgt as (
+select from_date, to_date
+from weights
+where from_date <='2017-01-01'
+and to_date >='2018-12-31'
+group by from_date, to_date
+having to_date-from_date=min(to_date-from_date)
+order by from_date
+limit 1),
+buyers as (
 select household_id, 1 as buyers
 from purchases
-group by household_id, pur_date, shop_id
+group by household_id
 )
 select count(*) buyers_unwgt, sum(w.weight*buyers) buyers_cs_wgt
-from weights w, buyers t
+from weights w, buyers t, cswgt cw
 where w.household_id=t.household_id
-and w.standard=0;
+and cw.from_date=w.from_date
+and cw.to_date=w.to_date;
 
 with avg_weights as (
 select household_id, sum(weight)/(select (julianday(max(to_date))-julianday(min(from_date))+1)/7 as weeks from weights where standard=1) as avg_weight
