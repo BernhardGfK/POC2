7f681a7c6764de58504ed8ee52d59792aa2aadba
bdschi <kadmus@nexgo.de>
2023-12-22 19:04:43 +0100

    table defintion was incomplete

--- artaxis.sql
+++ artaxis.sql
@@ -1,9 +1,9 @@
 drop table if exists artaxis;
 create table artaxis (
 	cat_id int,
-	cat_name int,
-	brnd_id,
-	brnd_name,
+	cat_name varchar(30),
+	brnd_id int,
+	brnd_name varchar(30),
 	postext varchar(60),
 	pos_id varchar(30),
 	parent_id varchar(30));
--- hhaxis.sql
+++ hhaxis.sql
@@ -1,9 +1,9 @@
 drop table if exists hhaxis;
 create table hhaxis (
 	bdl_id int,
-	bdl_name int,
-	age_id,
-	age_name,
+	bdl_name varchar(30),
+	age_id int,
+	age_name varchar(30),
 	postext varchar(60),
 	pos_id varchar(30),
 	parent_id varchar(30));
