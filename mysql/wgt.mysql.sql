016b537c610ac74e6c306653507f97ebfb780a31
bdschi <Bernhard.Schindler@gfk.com>
2023-12-30 15:25:06 +0100

    As files are created automatically -> remove them.

--- mysql/wgt.mysql.sql
+++
@@ -1,9 +0,0 @@
-drop table if exists weights;
-create table weights (
-	household_id int,
-	from_date date,
-	to_date date,
-	weight float,
-	standard int);
-load data local infile 'wgt.txt' into table weights;
-select * from weights limit 10;
